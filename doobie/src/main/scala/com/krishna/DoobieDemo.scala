134d0d250add65b6547fa921319a04a6df70500e
krishna-thapa <krishna.nature91@gmail.com>
2022-02-18 11:05:17 +0000

    Added case class and more methods using doobie

--- doobie/src/main/scala/com/krishna/DoobieDemo.scala
+++ doobie/src/main/scala/com/krishna/DoobieDemo.scala
@@ -1,6 +1,7 @@
 package com.krishna
 
 import cats.effect.{ ExitCode, IO, IOApp }
+import com.krishna.model.Actor
 import com.krishna.util.Helper.Debugger
 import doobie.implicits._
 import doobie.util.transactor.Transactor
@@ -14,15 +15,29 @@ object DoobieDemo extends IOApp {
     pass = "docker"
   )
 
-  def finaAllActorNames: IO[List[String]] = {
+  def findAllActorNames: IO[List[String]] = {
     val query = sql"select name from actors".query[String]
     val action = query.to[List]
     action.transact(xa)
   }
 
+  def findActorById(id: Int): IO[Actor] = {
+    val query = sql"select id, name from actors where id=$id".query[Actor]
+    val action = query.unique // will fail if record not found by that id
+    action.transact(xa)
+  }
+
+  def findActorByIdOption(id: Int): IO[Option[Actor]] = {
+    val query = sql"select id, name from actors where id=$id".query[Actor]
+    val action = query.option // will return None if record not found
+    action.transact(xa)
+  }
+
   override def run(args: List[String]): IO[ExitCode] = {
     //IO(println("Hello, doobie")).as(ExitCode.Success)
-    finaAllActorNames.debug.as(ExitCode.Success)
+    //finaAllActorNames.debug.as(ExitCode.Success)
+    //findActorById(1).debug.as(ExitCode.Success)
+    findActorByIdOption(99).debug.as(ExitCode.Success) // will return None
   }
 
 }
---
+++ doobie/src/main/scala/com/krishna/model/Movie.scala
@@ -0,0 +1,3 @@
+package com.krishna.model
+
+case class Movie(id: String, title: String, year: Int, actors: List[String], director: String)
